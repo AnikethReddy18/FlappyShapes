d72fbb63f2f51207db6a5561397236a15c3cc088
sunny <sunnyrockzzz1811@gmail.com>
2023-09-07 12:06:59 +0530

    Implementing player movement

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
 from pygame import (K_w,
                     K_s,
-                    K_LEFT,
-                    K_RIGHT,
+                    K_a,
+                    K_d,
                     K_ESCAPE,
                     KEYDOWN,
                     QUIT, )
@@ -13,20 +13,34 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.surf = pygame.Surface((100, 100))
         self.surf.fill((0, 0, 255))
-        self.surf_location = ((500 - 100) / 2, (650 - 100) / 2)
+        self.surf_location = ((SCREEN_WIDTH - 100) / 2, (SCREEN_LENGTH - 100) / 2)
         self.rect = self.surf.get_rect()
 
     def update_position(self, keys):
+
         if keys[K_w]:
             self.rect.move_ip(0, -1)
-
         if keys[K_s]:
             self.rect.move_ip(0, 1)
+        if keys[K_a]:
+            self.rect.move_ip(-1, 0)
+        if keys[K_d]:
+            self.rect.move_ip(1, 0)
 
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_LENGTH:
+            self.rect.bottom = SCREEN_LENGTH
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
 
 
 pygame.init()
 
+SCREEN_WIDTH = 500
+SCREEN_LENGTH = 650
 screen = pygame.display.set_mode([500, 650])
 
 player = Player()
