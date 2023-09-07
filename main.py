84204e27af34e16cba621dc991a96d3fdb779fc0
sunny <sunnyrockzzz1811@gmail.com>
2023-09-07 10:19:22 +0530

    Implementing player movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-from pygame import (K_UP,
-                    K_DOWN,
+from pygame import (K_w,
+                    K_s,
                     K_LEFT,
                     K_RIGHT,
                     K_ESCAPE,
@@ -16,6 +16,14 @@ class Player(pygame.sprite.Sprite):
         self.surf_location = ((500 - 100) / 2, (650 - 100) / 2)
         self.rect = self.surf.get_rect()
 
+    def update_position(self, keys):
+        if keys[K_w]:
+            self.rect.move_ip(0, -1)
+
+        if keys[K_s]:
+            self.rect.move_ip(0, 1)
+
+
 
 pygame.init()
 
@@ -25,13 +33,20 @@ player = Player()
 
 running = True
 while running:
+
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
 
-    screen.blit(player.surf, player.surf_location)
+    pressed_keys = pygame.key.get_pressed()
+    player.update_position(pressed_keys)
+
+    screen.fill((255, 255, 255))
+    screen.blit(player.surf, player.rect)
     pygame.display.flip()
 
 pygame.quit()
